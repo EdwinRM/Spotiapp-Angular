d7402080f12b2cb54a18a67add0d4a29d0835752
Edwin <edwinrmondragon@gmail.com>
2018-07-10 01:05:51 -0500

    consumiendo inf del servicio de Spotify

--- spotiapp/src/app/components/home/home.component.ts
+++ spotiapp/src/app/components/home/home.component.ts
@@ -1,13 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styles: []
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent{
 
-  constructor() { }
+ /* paises: any[] = [];
+
+  constructor (private http: HttpClient) {
+    //propiedades del objeto http,  peticiones get de un Api de paises
+    this.http.get('https://restcountries.eu/rest/v2/lang/es')//url con paises que hablan español 
+    .subscribe ((resp: any) => {
+      this.paises= resp; 
+      console.log(resp);
+    })
+   }*/
+
+   nuevasCanciones: any[] =[];
+
+   constructor(private spotify:SpotifyService){
+        
+    this.spotify.getNewReleases()
+    .subscribe( (data:any) => {
+       console.log(data.albums.items);
+
+       this.nuevasCanciones = data.albums.items;
+
+     });
+   }
 
   ngOnInit() {
   }
---
+++ spotiapp/src/app/services/spotify.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { variable } from '@angular/compiler/src/output/output_ast';
+
+@Injectable({ // decorador para importar el servicio de spotify automaticamente
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  constructor(private http: HttpClient) { 
+  console.log('Servicie listo Spotify');
+  }
+
+  getNewReleases(){
+    const headers = new HttpHeaders({
+      'Authorization':'Bearer BQB8Kmh3EIFm-JsK6WWbWcrxeKva43YsQ1FF_SDRsjvpUQHXUPMvpgK2KhZP7rBlZ3UJ8EGXYOuz5MCax8k' //token dado por spotify 1hr
+        });
+
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers });//peticion http.get
+  }
+}
