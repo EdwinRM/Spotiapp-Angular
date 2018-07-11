3fada8d0dad08c0952343838047dc84d46ee4260
Edwin <edwinrmondragon@gmail.com>
2018-07-10 23:16:05 -0500

    Buscador de artistas

--- spotiapp/src/app/services/spotify.service.ts
+++ spotiapp/src/app/services/spotify.service.ts
@@ -18,4 +18,12 @@ export class SpotifyService {
 
     return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers });//peticion http.get
   }
+getArtista( termino: string){
+  const headers = new HttpHeaders({
+    'Authorization':'Bearer BQB8Kmh3EIFm-JsK6WWbWcrxeKva43YsQ1FF_SDRsjvpUQHXUPMvpgK2KhZP7rBlZ3UJ8EGXYOuz5MCax8k' //token dado por spotify 1hr
+      });
+
+  return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, { headers }); 
+}
+
 }
