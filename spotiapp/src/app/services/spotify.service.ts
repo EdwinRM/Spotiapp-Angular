69c67985f5d9775e87e4b43e85f00d4f429eba54
Edwin <edwinrmondragon@gmail.com>
2018-07-19 23:33:15 -0500

    centralizar peticiones
    se creo constante

--- spotiapp/src/app/services/spotify.service.ts
+++ spotiapp/src/app/services/spotify.service.ts
@@ -14,24 +14,23 @@ export class SpotifyService {
   console.log('Servicie listo Spotify');
   }
 
-  getNewReleases(){
+  getQuery( query: string){
+    const url =`https://api.spotify.com/v1/${ query }`;
     const headers = new HttpHeaders({
       'Authorization':'Bearer BQBPiECgrkV3lk_TcvHlkw8JKUp2qXEP2dNfkThoNdWfncO7ZEl95Akt5UGxBebPQ8yofUKnJMmA-BaVhSjLSzVPgVLnXpUrNKR6wr51zOkFfH-yKhfzP2XbKDhwKu_MoPn8t90GVUg' //token dado por spotify 1hr
         });
-
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers })//peticion http.get
-    .pipe( map( data => {
-      return data['albums'].items;
-    }));
+        return this.http.get(url,{ headers });
   }
-getArtista( termino: string){
-  const headers = new HttpHeaders({
-    'Authorization':'Bearer BQBxwBdEMvVV2JeZ0-OFjtLX2yClMjGX0hrprLQKPnLKpSM5GOzTMHLEl7mAGxCZxp06vDx-NXChtDQxjPFgOvULJTWy03pd-CORGhP2KtXdsf33ilG0jchNmV9_RUWIx1QgdxNw5XE' //token dado por spotify 1hr
-      });
 
-  return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, { headers })
-  .pipe(map (data => data['artists'].items)
-  
+  getNewReleases(){  
+    return this.getQuery('browse/new-releases?limit=20')
+    .pipe( map( data => data['albums'].items));    
+  }
+
+  getArtista( termino: string){
+
+    return this.getQuery(`search?q=${termino}&type=artist&limit=15`)
+    .pipe( map ( data=> data['artists'].items));  
 }
 
 }
